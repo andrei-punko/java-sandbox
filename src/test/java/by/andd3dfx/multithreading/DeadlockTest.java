08369f8e526154aca01509a506683ee82610ab0e
Andrei Punko <andd3dfx@gmail.com>
2017-04-03 23:26:33 +0300

    Make deadlock by own hands

---
+++ src/test/java/by/andd3dfx/multithreading/DeadlockTest.java
@@ -0,0 +1,16 @@
+package by.andd3dfx.multithreading;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DeadlockTest {
+
+    /**
+     * Remove @Ignore to suggest that deadlock happened
+     */
+    @Ignore
+    @Test
+    public void main() throws Exception {
+        Deadlock.main(new String[]{});
+    }
+}
