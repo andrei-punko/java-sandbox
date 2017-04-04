facfcb2ccdc46b090d0ac1e0563336a77efdfade
Andrei Punko <andd3dfx@gmail.com>
2017-04-04 12:29:37 +0300

    Add ForkBlur example from JDK

---
+++ src/test/java/by/andd3dfx/multithreading/ForkBlurTest.java
@@ -0,0 +1,11 @@
+package by.andd3dfx.multithreading;
+
+import org.junit.Test;
+
+public class ForkBlurTest {
+
+    @Test
+    public void main() throws Exception {
+        ForkBlur.main(null);
+    }
+}
