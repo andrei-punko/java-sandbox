efed5adec82c7154f8d081d1e21a3e73d42d4bfe
Andrei Punko <Andrei_Punko@epam.com>
2018-01-26 12:57:33 +0300

    Extract some repeatable logic

--- src/test/java/by/andd3dfx/interview/synesis/CustomCacheTest.java
+++ src/test/java/by/andd3dfx/interview/synesis/CustomCacheTest.java
@@ -6,33 +6,36 @@ import static java.lang.Math.random;
 import static java.lang.Thread.sleep;
 
 public class CustomCacheTest {
-    private final int COUNT = 1000;
+    private final int COUNT = 100;
 
     @Test
     public void testCache() throws InterruptedException {
         CustomCache customCache = new CustomCache();
         for (int i = 0; i < COUNT; i++) {
-            customCache.write(i, i * i + 5);
+            customCache.write(i, func(i));
         }
 
         Thread readingThread1 = new Thread(() -> {
             while (true) {
+                delay();
                 int index = (int) (COUNT * random());
-                System.out.println("Read thread 1, A[" + index + "] = " + customCache.read(index));
+                System.out.println("Read thread 1, A[" + index + "] -> " + customCache.read(index));
             }
         });
         Thread readingThread2 = new Thread(() -> {
             while (true) {
+                delay();
                 int index = (int) (COUNT * random());
-                System.out.println("Read thread 2, A[" + index + "] = " + customCache.read(index));
+                System.out.println("Read thread 2, A[" + index + "] -> " + customCache.read(index));
             }
         });
         Thread writingThread = new Thread(() -> {
             while (true) {
+                delay();
                 int index = (int) (COUNT * random());
-                int value = index * 10 - 5;
+                int value = func(index);
                 customCache.write(index, value);
-                System.out.println("Write thread, A[" + index + "] := " + value);
+                System.out.println("Write thread, A[" + index + "] <- " + value);
             }
         });
 
@@ -40,6 +43,18 @@ public class CustomCacheTest {
         readingThread2.start();
         writingThread.start();
 
-        sleep(10);
+        sleep(200);
+    }
+
+    private void delay() {
+        try {
+            sleep((long) (10 * random()));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int func(int arg) {
+        return arg * arg + 5;
     }
 }
