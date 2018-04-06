0baef1ab81a06613fa08c108d4970461825cd9f4
Andrei Punko <Andrei_Punko@epam.com>
2018-04-06 13:57:49 +0300

    Solve Chicken task from WF

--- src/main/java/by/andd3dfx/interview/wf/test/Chicken.java
+++ src/main/java/by/andd3dfx/interview/wf/test/Chicken.java
@@ -7,24 +7,45 @@ interface IBird {
   Egg lay();
 }
 
-public class Chicken {
+public class Chicken implements IBird {
 
-  public Chicken() {
+  @Override
+  public Egg lay() {
+    return new Egg(() -> new Chicken());
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Chicken chicken = new Chicken();
+    Egg egg = chicken.lay();
+    IBird hatchedBird = egg.hatch();
+
     System.out.println(chicken instanceof IBird);
+    System.out.println(hatchedBird instanceof Chicken);
+  }
+}
+
+class Goose implements IBird {
+
+  @Override
+  public Egg lay() {
+    return new Egg(() -> new Goose());
   }
 }
 
 class Egg {
 
+  private final Callable<IBird> createBird;
+  private boolean isHatched = false;
+
   public Egg(Callable<IBird> createBird) {
-    throw new UnsupportedOperationException();
+    this.createBird = createBird;
   }
 
-  public IBird hatch() {
-    throw new UnsupportedOperationException();
+  public IBird hatch() throws Exception {
+    if (isHatched) {
+      throw new IllegalStateException("This egg already hatched!");
+    }
+    isHatched = true;
+    return createBird.call();
   }
 }
