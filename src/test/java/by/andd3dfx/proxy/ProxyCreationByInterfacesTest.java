b1dd7e30ac33998d24e17acb5ea5b97928aa6c7d
Andrei Punko <Andrei_Punko@epam.com>
2018-06-04 13:56:51 +0300

    Add example of dynamic proxy usage

---
+++ src/test/java/by/andd3dfx/proxy/ProxyCreationByInterfacesTest.java
@@ -0,0 +1,22 @@
+package by.andd3dfx.proxy;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.lang.reflect.Proxy;
+import org.junit.Test;
+
+public class ProxyCreationByInterfacesTest {
+
+  @Test
+  public void performAction() {
+    Object proxyInstance = Proxy.newProxyInstance(
+        ProxyCreationByInterfacesTest.class.getClassLoader(),
+        new Class[]{SomeInterface1.class, SomeInterface2.class},
+        new DynamicInvocationHandler()
+    );
+
+    assertThat("Double value expected", ((SomeInterface1) proxyInstance).performActionOne(10), is(20));
+    assertThat("Square value expected", ((SomeInterface2) proxyInstance).performActionTwo(10), is(100));
+  }
+}
