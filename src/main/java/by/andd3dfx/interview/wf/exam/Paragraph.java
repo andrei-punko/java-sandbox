b8cfcdd32927af857be2388effd0dc2cd3efefed
Andrei Punko <andd3dfx@gmail.com>
2019-01-23 01:41:22 +0300

    Add some regexp examples

--- src/main/java/by/andd3dfx/interview/wf/exam/Paragraph.java
+++ src/main/java/by/andd3dfx/interview/wf/exam/Paragraph.java
@@ -19,8 +19,17 @@ public class Paragraph {
     return paragraph;
   }
 
+  public static String changeFormatAnotherWay(String paragraph) {
+    Pattern p = Pattern.compile("(\\d{3}[^-]*)-([^-]*\\d{2}[^-]*)-([^-]*\\d{4})");
+    Matcher m = p.matcher(paragraph);
+    while (m.find()) {
+      paragraph = m.replaceAll("$1/$3/$2");
+    }
+    return paragraph;
+  }
+
   public static void main(String[] args) {
-    System.out.println(changeFormat("Please quote your policy number: 112-39-8552."));
-    System.out.println(changeFormat("Please quote your policy number: 112-39-\n8552."));
+    System.out.println(changeFormatAnotherWay("Please quote your policy number: 112-39-8552."));
+    System.out.println(changeFormatAnotherWay("Please quote your policy number: 112-39-\n8552."));
   }
 }
--- src/test/java/by/andd3dfx/interview/wf/exam/ParagraphTest.java
+++ src/test/java/by/andd3dfx/interview/wf/exam/ParagraphTest.java
@@ -1,6 +1,7 @@
 package by.andd3dfx.interview.wf.exam;
 
 import static by.andd3dfx.interview.wf.exam.Paragraph.changeFormat;
+import static by.andd3dfx.interview.wf.exam.Paragraph.changeFormatAnotherWay;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -29,6 +30,30 @@ public class ParagraphTest {
   @Test
   public void changeFormatWithNewStringDelimeter() {
     assertThat("Unexpected string", changeFormat("Please quote your policy number: 112-39-\n8552."),
+        is("Please quote your policy number: 112/\n8552/39."));
+  }
+
+  @Test
+  public void changeFormatAnotherWayNothingChanged() {
+    assertThat("Unexpected string", changeFormatAnotherWay("Please quote your policy number."),
+        is("Please quote your policy number."));
+  }
+
+  @Test
+  public void changeFormatAnotherWaySimple() {
+    assertThat("Unexpected string", changeFormatAnotherWay("Please quote your policy number: 112-39-8552."),
+        is("Please quote your policy number: 112/8552/39."));
+  }
+
+  @Test
+  public void changeFormatAnotherWayMultipleParagraphs() {
+    assertThat("Unexpected string", changeFormatAnotherWay("Please quote your policy numbers: 112-39-8552 and 134-39-9552."),
+        is("Please quote your policy numbers: 112/8552/39 and 134/9552/39."));
+  }
+
+  @Test
+  public void changeFormatAnotherWayWithNewStringDelimeter() {
+    assertThat("Unexpected string", changeFormatAnotherWay("Please quote your policy number: 112-39-\n8552."),
         is("Please quote your policy number: 112/\n8552/39."));
   }
 }
