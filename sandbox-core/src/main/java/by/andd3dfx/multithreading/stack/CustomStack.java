ac8e52835d2bf70f9ca6411c9c32dabe7d6c13d8
Andrei Punko <andd3dfx@gmail.com>
2019-04-19 21:41:07 +0300

    Add stack with multithreading support without using blocking operations

---
+++ sandbox-core/src/main/java/by/andd3dfx/multithreading/stack/CustomStack.java
@@ -0,0 +1,51 @@
+package by.andd3dfx.multithreading.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Implement custom stack with multithreading support without using blocking operations
+ */
+public class CustomStack<T> {
+
+    private Deque<T> deque = new ArrayDeque<>();
+    private ReentrantLock lock = new ReentrantLock();
+
+    public T push(T element) {
+        lock.lock();
+        try {
+            deque.push(element);
+            return element;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T pop() {
+        lock.lock();
+        try {
+            return deque.pop();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T peek() {
+        lock.lock();
+        try {
+            return deque.peek();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean isEmpty() {
+        lock.lock();
+        try {
+            return deque.isEmpty();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
