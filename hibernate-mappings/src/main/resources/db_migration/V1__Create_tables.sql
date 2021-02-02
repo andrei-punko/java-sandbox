a8e34e910e454c0e8dfd87dfe76b721478276469
Andrei Punko <andd3dfx@gmail.com>
2021-02-02 12:53:58 +0300

    Adjust sql scripts formatting

--- hibernate-mappings/src/main/resources/db_migration/V1__Create_tables.sql
+++ hibernate-mappings/src/main/resources/db_migration/V1__Create_tables.sql
@@ -1,3 +1,4 @@
+
 create table AUTHOR
 (
   id   int          not null auto_increment,
--- hibernate-mappings/src/main/resources/db_migration/V3__Create_and_populate_tables_with_inheritance.sql
+++ hibernate-mappings/src/main/resources/db_migration/V3__Create_and_populate_tables_with_inheritance.sql
@@ -12,11 +12,7 @@ create table ITEMS
   primary key (id)
 );
 
-insert into ITEMS (item_type, name, price, weight, size_1, size_2, size_3)
-values ('BOX', 'Milk packet', 1.5, 1, 15, 5, 10);
-insert into ITEMS (item_type, name, price, weight, size_1, size_2)
-values ('TUBE', 'Iron tube', 29.9, 100, 600, 15);
-insert into ITEMS (item_type, name, price, weight, size_1, size_2)
-values ('TUBE', 'Copper tube', 19.5, 140, 200, 12);
-insert into ITEMS (item_type, name, price, weight)
-values ('ITEM', 'Toy house', 4.9, 0.05);
+insert into ITEMS (item_type, name, price, weight, size_1, size_2, size_3) values ('BOX', 'Milk packet', 1.5, 1, 15, 5, 10);
+insert into ITEMS (item_type, name, price, weight, size_1, size_2) values ('TUBE', 'Iron tube', 29.9, 100, 600, 15);
+insert into ITEMS (item_type, name, price, weight, size_1, size_2) values ('TUBE', 'Copper tube', 19.5, 140, 200, 12);
+insert into ITEMS (item_type, name, price, weight) values ('ITEM', 'Toy house', 4.9, 0.05);
