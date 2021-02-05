41344a3abd2fcace02238e4abd004e12e81ba6ff
Andrei Punko <andd3dfx@gmail.com>
2021-02-05 11:57:17 +0300

    Fix EquivalentTrees a bit: add support of some test cases

--- sandbox-core/src/main/java/by/andd3dfx/tree/EquivalentTrees.java
+++ sandbox-core/src/main/java/by/andd3dfx/tree/EquivalentTrees.java
@@ -3,7 +3,6 @@ package by.andd3dfx.tree;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,7 +18,6 @@ import java.util.stream.Collectors;
  * Node left;
  * Node right;
  * };
- * Node[] FindEquivalentSubtrees(Node root);
  */
 public class EquivalentTrees {
 
@@ -37,6 +35,15 @@ public class EquivalentTrees {
             this.left = left;
             this.right = right;
         }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
     }
 
     public List<Node> findEquivalentSubtrees(Node root) {
@@ -44,11 +51,11 @@ public class EquivalentTrees {
             return null;
         }
 
-        // Node -> Vocabulary
+        // Build Node -> Vocabulary map
         Map<Node, Set<Character>> node2Voc = new HashMap<>();
         buildNode2Voc(root, node2Voc);
 
-        // Set<Character> -> List<Node>
+        // Build Set<Character> -> List<Node> map
         Map<Set<Character>, List<Node>> voc2Nodes = new HashMap<>();
         for (Node node : node2Voc.keySet()) {
             Set<Character> value = node2Voc.get(node);
@@ -66,17 +73,20 @@ public class EquivalentTrees {
                         entry -> entry.getKey(),
                         entry -> entry.getValue()
                 ));
-        // Only absent sets with at least 2 related nodes
+        // Only absent sets with at least 2 related nodes remain
 
         if (voc2Nodes.isEmpty()) {
             return null;
         }
 
-        // TODO: UNFINISHED - find nodes with max size of subtree
+        // Build Node->nodeSize map
+        Map<Node, Integer> node2Size = new HashMap<>();
+        buildNode2Size(root, node2Size);
+
+        // TODO: UNFINISHED: in each list of voc2Nodes.value - find two nodes with max sum size of subtree. after that - find pair with max sum
 
         // Until task not finished - just return first element
-        Node nodes = voc2Nodes.keySet().toArray(new Node[0])[0];
-        return voc2Nodes.get(nodes);
+        return (List<Node>) voc2Nodes.values().toArray()[0];
     }
 
     private Set<Character> buildNode2Voc(Node node, Map<Node, Set<Character>> node2Voc) {
@@ -93,5 +103,21 @@ public class EquivalentTrees {
         }
 
         return node2Voc.get(node);
+    }
+
+    private int buildNode2Size(Node node, Map<Node, Integer> node2Size) {
+        if (!node2Size.containsKey(node)) {
+            node2Size.put(node, 0);
+        }
+        node2Size.put(node, node2Size.get(node) + 1);
+
+        if (node.left != null) {
+            node2Size.put(node, node2Size.get(node) + buildNode2Size(node.left, node2Size));
+        }
+        if (node.right != null) {
+            node2Size.put(node, node2Size.get(node) + buildNode2Size(node.right, node2Size));
+        }
+
+        return node2Size.get(node);
     }
 }
--- sandbox-core/src/test/java/by/andd3dfx/tree/EquivalentTreesTest.java
+++ sandbox-core/src/test/java/by/andd3dfx/tree/EquivalentTreesTest.java
@@ -3,6 +3,9 @@ package by.andd3dfx.tree;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,12 +31,45 @@ public class EquivalentTreesTest {
     }
 
     @Test
-    public void findEquivalentSubtrees() {
+    public void findEquivalentSubtreesWhenNoCandidates() {
         EquivalentTrees.Node node = new EquivalentTrees.Node('A');
         node.left = new EquivalentTrees.Node('B');
         assertThat(equivalentTrees.findEquivalentSubtrees(node), is(nullValue()));
+    }
 
-        node.right = new EquivalentTrees.Node('C');
-        assertThat(equivalentTrees.findEquivalentSubtrees(node), is(nullValue()));
+    @Test
+    public void findEquivalentSubtreesWhenOneCandidate() {
+        EquivalentTrees.Node node = new EquivalentTrees.Node('A');
+        node.left = new EquivalentTrees.Node('B');
+        node.right = new EquivalentTrees.Node('B');
+
+        List<EquivalentTrees.Node> result = equivalentTrees.findEquivalentSubtrees(node);
+
+        assertThat(result.size(), is(2));
+        assertThat(result, hasItems(node.left, node.right));
+    }
+
+    @Test
+    public void findEquivalentSubtreesWhenManyCandidatesExist() {
+        EquivalentTrees.Node root = buildNode('A');
+        root.left = buildNode('B');
+        root.right = buildNode('C');
+
+        root.left.left = buildNode('E');
+        root.left.left.left = buildNode('D');
+
+        root.right.left = buildNode('D');
+        root.right.right = buildNode('E');
+        root.right.right.left = buildNode('E');
+        root.right.right.right = buildNode('D');
+
+        List<EquivalentTrees.Node> result = equivalentTrees.findEquivalentSubtrees(root);
+
+        assertThat("Two nodes expected", result.size(), is(2));
+        assertThat("Wrong pair of nodes", result, hasItems(root.left, root.right));
+    }
+
+    private EquivalentTrees.Node buildNode(char value) {
+        return new EquivalentTrees.Node(value);
     }
 }
