24037b90e36c7c7442b2e60fc0f710e3bad2ce27
Andrei Punko <andd3dfx@gmail.com>
2021-07-25 20:17:12 +0300

    Adjust bat files according to current jar names

--- sound-recorder-n-spectrum-analyzer/sound-recorder.bat
+++ sound-recorder-n-spectrum-analyzer/sound-recorder.bat
@@ -1,2 +1,2 @@
 
-java -cp ./target/sound-spectrum-analyzer-1.0-SNAPSHOT-jar-with-dependencies.jar by.andd3dfx.capturesound.AudioCaptureApp
+java -cp ./target/sound-recorder-n-spectrum-analyzer-1.0-SNAPSHOT-jar-with-dependencies.jar by.andd3dfx.capturesound.AudioCaptureApp
