f5c35ec7a6b4402e9236c09ba35436a27ea495b0
Andrei Punko <andd3dfx@gmail.com>
2024-05-14 13:48:44 +0300

    #80 Fix build issue on Win OS

--- hibernate-mappings/src/test/java/by/andd3dfx/HibernateMainIT.java
+++ hibernate-mappings/src/test/java/by/andd3dfx/HibernateMainIT.java
@@ -1,54 +1,68 @@
 package by.andd3dfx;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
 import by.andd3dfx.model.library.Address;
 import by.andd3dfx.model.library.Author;
 import by.andd3dfx.model.library.Book;
 import by.andd3dfx.model.library.Publisher;
+import by.andd3dfx.model.shop.OneSizeItem;
+import by.andd3dfx.model.shop.ThreeSizeItem;
+import by.andd3dfx.model.shop.TwoSizeItem;
+import by.andd3dfx.model.shop.ZeroSizeItem;
 import lombok.SneakyThrows;
-import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HibernateMainIT {
 
     @SneakyThrows
     @Test
-    public void checkRecordsExistence() {
+    public void checkRecordsAmount() {
         try (var sessionFactory = new Configuration().configure().buildSessionFactory()) {
             try (var session = sessionFactory.openSession()) {
-                getFromDbAndCheck(session, Book.class, 4);
-                getFromDbAndCheck(session, Address.class, 2);
-                getFromDbAndCheck(session, Author.class, 3);
-                getFromDbAndCheck(session, Publisher.class, 2);
+                var emFactory = session.getEntityManagerFactory();
+                var em = emFactory.createEntityManager();
 
-                EntityManager entityManager = session.getEntityManagerFactory().createEntityManager();
-                getFromDbAndCheck(entityManager, "ZeroSizeItem", 1);
-                getFromDbAndCheck(entityManager, "OneSizeItem", 1);
-                getFromDbAndCheck(entityManager, "TwoSizeItem", 2);
-                getFromDbAndCheck(entityManager, "ThreeSizeItem", 1);
+                getFromDbAndCheck(em, Book.class, 4);
+                getFromDbAndCheck(em, Address.class, 2);
+                getFromDbAndCheck(em, Author.class, 3);
+                getFromDbAndCheck(em, Publisher.class, 2);
+
+                getFromDbAndCheck_ForEntityWithDiscriminator(em, ZeroSizeItem.class, 1);
+                getFromDbAndCheck_ForEntityWithDiscriminator(em, OneSizeItem.class, 1);
+                getFromDbAndCheck_ForEntityWithDiscriminator(em, TwoSizeItem.class, 2);
+                getFromDbAndCheck_ForEntityWithDiscriminator(em, ThreeSizeItem.class, 1);
             }
         }
     }
 
-    private void getFromDbAndCheck(Session session, Class clazz, int expectedItemsCount) {
-        List items = session.createCriteria(clazz).list();
-        System.out.println("Retrieved next items: " + items);
+    private <T> void getFromDbAndCheck(EntityManager em, Class<T> clazz, int expectedItemsCount) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(clazz);
+        Root<T> variableRoot = query.from(clazz);
+        query.select(variableRoot);
+        var items = em.createQuery(query).getResultList();
 
+        System.out.println("Retrieved next items: " + items);
         assertThat("Wrong items count", items.size(), is(expectedItemsCount));
     }
 
-    private void getFromDbAndCheck(EntityManager entityManager, String entityName, int expectedItemsCount) {
-        Query query = entityManager.createQuery("SELECT c FROM " + entityName + " c");
-        List items = query.getResultList();
-        System.out.println("Retrieved next items: " + items);
+    private <T> void getFromDbAndCheck_ForEntityWithDiscriminator(EntityManager em, Class<T> clazz, int expectedItemsCount) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(clazz);
+        Root<T> variableRoot = query.from(clazz);
+        query.where(builder.equal(variableRoot.type(), clazz));
+        query.select(variableRoot);
+        var items = em.createQuery(query).getResultList();
 
+        System.out.println("Retrieved next items: " + items);
         assertThat("Wrong items count", items.size(), is(expectedItemsCount));
     }
 }
