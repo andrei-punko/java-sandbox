5758789d472e7924d5feca683227d10c82a6e890
Andrei Punko <andd3dfx@gmail.com>
2024-12-12 12:02:06 +0300

    Switch hibernate-mappings module from MySQL to PostgreSQL

--- hibernate-mappings/src/main/resources/db_migration/V3__Create_tables_with_inheritance.sql
+++ hibernate-mappings/src/main/resources/db_migration/V3__Create_tables_with_inheritance.sql
@@ -1,6 +1,6 @@
 
 create table ITEMS (
-  ID          int           not null primary key auto_increment,
+  ID          serial        not null primary key,
   NAME        varchar(100)  not null,
   PRICE       decimal(5, 2) not null,
   WEIGHT      decimal(5, 2) not null,
