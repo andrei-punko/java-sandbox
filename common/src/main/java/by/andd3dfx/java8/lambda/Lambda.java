743d1dd4ddf2165958c5acbf98fd050a32563a68
Andrei Punko <andd3dfx@gmail.com>
2025-12-23 21:05:11 +0300

    Обновлен устаревший API в Lambda.java: заменен Collections.sort() на List.sort()

--- common/src/main/java/by/andd3dfx/java8/lambda/Lambda.java
+++ common/src/main/java/by/andd3dfx/java8/lambda/Lambda.java
@@ -1,13 +1,12 @@
 package by.andd3dfx.java8.lambda;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Lambda {
 
     public void usualApproach(List<String> names) {
-        Collections.sort(names, new Comparator<String>() {
+        names.sort(new Comparator<String>() {
             @Override
             public int compare(String a, String b) {
                 return a.compareTo(b);
@@ -16,16 +15,16 @@ public class Lambda {
     }
 
     public void lambda1(List<String> names) {
-        Collections.sort(names, (String a, String b) -> {
+        names.sort((String a, String b) -> {
             return a.compareTo(b);
         });
     }
 
     public void lambda2(List<String> names) {
-        Collections.sort(names, (String a, String b) -> a.compareTo(b));
+        names.sort((String a, String b) -> a.compareTo(b));
     }
 
     public void lambda3(List<String> names) {
-        Collections.sort(names, (a, b) -> a.compareTo(b));
+        names.sort((a, b) -> a.compareTo(b));
     }
 }
